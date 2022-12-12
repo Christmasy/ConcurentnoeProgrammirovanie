1b2327676288a5918fcaf746b399159297851f2a
Christmasy <istominachr2002@gmail.com>
2022-12-12 21:54:36 +0500

    task4 done

--- Program.cs
+++ Program.cs
@@ -1,69 +1,134 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
-namespace ContKor
+namespace CustomThreadPool
 {
-    public interface IStack<T>
+    public static class Program
     {
-        void Push(T item);
-        bool TryPop(out T item);
-        int Count { get; }
+        public static void Main()
+        {
+            //ThreadPoolTests.Run<DotNetThreadPoolWrapper>();
+            ThreadPoolTests.Run<ThreadPool>();
+        }
     }
-    
-    public class MyStack<T> : IStack<T>
+
+    public class ThreadPool : IThreadPool, IDisposable
     {
-        private Node _head;
+        private readonly Worker[] _workers;
+        private readonly Queue<Action> _globalQueue;
+        private long _finishedTask;
+        private volatile int _quantityOfWaitThreads;
+        public ThreadPool() : this(Environment.ProcessorCount) { }
 
-        public int Count 
-        { 
-            get
-            {
-                if (_head == null) return 0;
-                return _head.Quantity; 
-            } 
-        }
-
-        public void Push(T item)
+        private ThreadPool(int threadCount)
         {
-            var spin = new SpinWait();
-            while (true)
+            _globalQueue = new Queue<Action>();
+            if (threadCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(threadCount));
+            _workers = Enumerable.Range(0, threadCount).Select(_ => 
             {
-                var previousHead = _head;
-                var quantity = 1;
-                if (previousHead != null) quantity += previousHead.Quantity;
-                var node = new Node(item, previousHead, quantity);
-                if (Interlocked.CompareExchange(ref _head, node, previousHead) == previousHead) return;
-                spin.SpinOnce();
+                var worker = new Worker(DispatchLoop);
+                worker.Thread.Start();
+                return worker;
             }
+            ).ToArray();
+        }
+        
+        public void EnqueueAction(Action action)
+        {
+            if (Worker.CurrentWorker.Value != null) LocalQueuePushTask(action);
+            else
+                lock (_globalQueue)
+                    _globalQueue.Enqueue(action);
+            if (_quantityOfWaitThreads <= 0) return;
+
+            lock (_globalQueue)
+                Monitor.Pulse(_globalQueue);
         }
 
-        public bool TryPop(out T item)
+        public long GetTasksProcessedCount() => _finishedTask;
+
+        private static void LocalQueuePushTask(Action action)
         {
-            item = default;
-            var spin = new SpinWait();
+            if(Worker.CurrentWorker.Value != null)
+                Worker.CurrentWorker.Value!.LocalQueue.LocalPush(action);
+            else throw new InvalidOperationException("This thread is not a worker");
+        }
+
+        private void DispatchLoop(Worker worker)
+        {
             while (true)
             {
-                if (_head == null) return false;
-                var oldHead = _head;
-                if (Interlocked.CompareExchange(ref _head, oldHead.Next, oldHead) == oldHead)
+                GetTask().Invoke();
+                Interlocked.Increment(ref _finishedTask);
+            }
+
+            Action GetTask()
+            {
+                if (GetTaskFromLocalQueue(worker, out var task))
+                    return task!;
+                while (true)
                 {
-                    item = oldHead.Value;
-                    return true;
+                    bool canGetFromGlobal;
+                    lock (_globalQueue)
+                        canGetFromGlobal = _globalQueue.TryDequeue(out task);
+                    if (canGetFromGlobal || StealTask(worker, out task))
+                        return task!;
+                    lock (_globalQueue)
+                    {
+                        _quantityOfWaitThreads++;
+                        try
+                        {
+                            Monitor.Wait(_globalQueue);
+                        }
+                        finally
+                        {
+                            _quantityOfWaitThreads--;
+                        }
+                    }
                 }
-                spin.SpinOnce();
             }
         }
 
-        private class Node
+        bool GetTaskFromLocalQueue(Worker worker, out Action? task)
         {
-            public readonly T Value;
-            public readonly Node Next;
-            public readonly int Quantity;
-            public Node(T value, Node next, int quantity)
+            task = null;
+            return worker.LocalQueue.LocalPop(ref task!);
+        }
+
+        bool StealTask(Worker worker, out Action? task)
+        {
+            task = null;
+            var workersExceptWorker = _workers?.Where(w => w != worker) ?? Enumerable.Empty<Worker>();
+            foreach (var w in workersExceptWorker)
+                if (w.LocalQueue.TrySteal(ref task!))
+                    return true;
+            return false;
+        }
+
+        public void Dispose() => _workers.ToList().ForEach(w => w.Dispose());
+
+        private class Worker : IDisposable
+        {
+            public static readonly ThreadLocal<Worker> CurrentWorker = new ThreadLocal<Worker>();
+            public WorkStealingQueue<Action> LocalQueue { get; } = new WorkStealingQueue<Action>();
+            public Thread Thread { get; }
+            public Worker(Action<Worker> dispatchLoop)
             {
-                Value = value;
-                Next = next;
-                Quantity = quantity;
+                Thread = new Thread(ThreadFunc)
+                {
+                    IsBackground = true
+                };
+
+                void ThreadFunc()
+                {
+                    CurrentWorker.Value = this;
+                    dispatchLoop(this);
+                }
             }
+            public void Dispose() => CurrentWorker.Dispose();
         }
     }
 }
